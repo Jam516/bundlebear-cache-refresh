963a747612a615957080b84fc7a039d6fc282d9f
Jam516 <kufuorkofi@gmail.com>
2024-11-29 13:31:44 +0000

    commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 class BundleBearAPI:
     def __init__(self, base_url: str):
         self.base_url = base_url.rstrip('/')
-        self.chains = ['all', 'ethereum', 'optimism', 'arbitrum', 'base', 'polygon', 'avalanche']
+        self.chains = ['all', 'ethereum', 'optimism', 'arbitrum', 'base', 'polygon', 'avalanche', 'bsc']
         self.endpoints = [
             '/overview',
             '/bundler',
